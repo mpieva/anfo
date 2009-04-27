mpieva/anfo

commit 5d2b17545d6abdd912c25cf1e6433202e964c267

double checked penalty calculations and corrected scoring of overhangs
git-svn-id: http://biofs04/svn/bioinf/anfo/tags/anfo-0.9.0@1681 93d9a472-daf7-0310-8f21-d7befa554562

align.h
@@ -488,11 +488,12 @@ template< typename F > void forward( const simple_adna& s, F f )
 		while( s1.get_qry().ambicode )
 		{
 			s1.penalty += s1.subst_penalty() ;
+			if( s.state & simple_adna::mask_ss ) s.penalty += simple_adna::overhang_ext_penalty ;
 			s1.adv_qry() ;
 			f( s1 ) ;
 		}
 	}
-	else if( (s.state & simple_adna::mask_dir) == 0 && ( s.get_ref() == 0 || s.get_qry().ambicode == 0 ) )
+	else if( (s.state & simple_adna::mask_dir) == 0 && !s.get_ref() )
 	{
 		// forward dir, hit gap --> start over in reverse dir
 		simple_adna s1 = s ;
@@ -533,7 +534,8 @@ template< typename F > void forward( const simple_adna& s, F f )
 			// rates in aDNA.
 			simple_adna s4 = s ;
 			s4.state |= simple_adna::mask_ss ;
-			uint32_t p4 = s4.subst_penalty() + simple_adna::overhang_enter_penalty ;
+			uint32_t p4 = s4.subst_penalty() + simple_adna::overhang_enter_penalty 
+			                                 + simple_adna::overhang_ext_penalty ;
 			uint32_t p0 = s.subst_penalty() ;
 			if( p4 < p0 ) {
 				s4.penalty += p4 ;

configure
@@ -1,6 +1,6 @@
 #! /bin/sh
 # Guess values for system-dependent variables and create Makefiles.
-# Generated by GNU Autoconf 2.61 for anfo 0.9.0.
+# Generated by GNU Autoconf 2.61 for anfo 0.9.0.1.
 #
 # Report bugs to <udo_stenzel@eva.mpg.de>.
 #
@@ -574,8 +574,8 @@ SHELL=${CONFIG_SHELL-/bin/sh}
 # Identity of this package.
 PACKAGE_NAME='anfo'
 PACKAGE_TARNAME='anfo'
-PACKAGE_VERSION='0.9.0'
-PACKAGE_STRING='anfo 0.9.0'
+PACKAGE_VERSION='0.9.0.1'
+PACKAGE_STRING='anfo 0.9.0.1'
 PACKAGE_BUGREPORT='udo_stenzel@eva.mpg.de'
 
 ac_unique_file="util.cc"
@@ -1189,7 +1189,7 @@ if test "$ac_init_help" = "long"; then
   # Omit some internal or obsolete options to make the list less imposing.
   # This message is too long to be a string in the A/UX 3.1 sh.
   cat <<_ACEOF
-\`configure' configures anfo 0.9.0 to adapt to many kinds of systems.
+\`configure' configures anfo 0.9.0.1 to adapt to many kinds of systems.
 
 Usage: $0 [OPTION]... [VAR=VALUE]...
 
@@ -1250,7 +1250,7 @@ fi
 
 if test -n "$ac_init_help"; then
   case $ac_init_help in
-     short | recursive ) echo "Configuration of anfo 0.9.0:";;
+     short | recursive ) echo "Configuration of anfo 0.9.0.1:";;
    esac
   cat <<\_ACEOF
 
@@ -1330,7 +1330,7 @@ fi
 test -n "$ac_init_help" && exit $ac_status
 if $ac_init_version; then
   cat <<\_ACEOF
-anfo configure 0.9.0
+anfo configure 0.9.0.1
 generated by GNU Autoconf 2.61
 
 Copyright (C) 1992, 1993, 1994, 1995, 1996, 1998, 1999, 2000, 2001,
@@ -1344,7 +1344,7 @@ cat >config.log <<_ACEOF
 This file contains any messages produced by compilers while
 running configure, to aid debugging if configure makes a mistake.
 
-It was created by anfo $as_me 0.9.0, which was
+It was created by anfo $as_me 0.9.0.1, which was
 generated by GNU Autoconf 2.61.  Invocation command line was
 
   $ $0 $@
@@ -8051,7 +8051,7 @@ exec 6>&1
 # report actual input values of CONFIG_FILES etc. instead of their
 # values after options handling.
 ac_log="
-This file was extended by anfo $as_me 0.9.0, which was
+This file was extended by anfo $as_me 0.9.0.1, which was
 generated by GNU Autoconf 2.61.  Invocation command line was
 
   CONFIG_FILES    = $CONFIG_FILES
@@ -8104,7 +8104,7 @@ Report bugs to <bug-autoconf@gnu.org>."
 _ACEOF
 cat >>$CONFIG_STATUS <<_ACEOF
 ac_cs_version="\\
-anfo config.status 0.9.0
+anfo config.status 0.9.0.1
 configured by $0, generated by GNU Autoconf 2.61,
   with options \\"`echo "$ac_configure_args" | sed 's/^ //; s/[\\""\`\$]/\\\\&/g'`\\"
 

configure.ac
@@ -2,7 +2,7 @@
 # Process this file with autoconf to produce a configure script.
 
 AC_PREREQ(2.61)
-AC_INIT([anfo], [0.9.0], [udo_stenzel@eva.mpg.de])
+AC_INIT([anfo], [0.9.0.1], [udo_stenzel@eva.mpg.de])
 AC_CONFIG_SRCDIR([util.cc])
 AC_CONFIG_HEADER([config.h])
 

align.cc
@@ -43,20 +43,29 @@ namespace {
 //! For ambiguity codes, we sum over all the possible codes, divide by
 //! the number of possibilities, and later (when aligning) go to the log
 //! domain.  Note that matches end up costing nothing (almost nothing,
-//! but accuracy isn't good enough to pick that up), matching to an N
-//! *does* cost something, and matching a T to a C might be dirt cheap.
+//! but accuracy isn't good enough to pick up on that), matching to an N
+//! *does* cost something, and matching a T to a C is dirt cheap while
+//! we believe we are in single stranded DNA.
 //!
 //! For gaps, we already have probabilities.  Taking the log is all it
 //! takes to make scores from that.
 //!
-//! For the overhang, we want something that's easy to parameterize.  We
-//! assume a geometric distribution of overhang length with parameter p,
-//! but half the overhangs end up pointing in the wrong direction, which
-//! maps them to zero length.  The mean overhang length ends up being
-//! (1-p)/(2p), and we take that as parameter.  Entering an overhang
-//! involves a penalty (since the probability is less than one), but so
-//! does not entering it.  We simply take the difference of the two,
-//! leaving the penalty for a perfect alignment at zero.
+//! For the overhang, we want something that's easy to parameterize, and
+//! the single parameter will be the average overhang length.  We assume
+//! there's an even chance that we have a 5' overhang (as opposed to a
+//! 3' overhang), and if we have a 5' overhang, it's length follows a
+//! geometric distribution with parameter p.
+//! 
+//! The mean overhang length ends up being (1-p)/(2p), which is just
+//! half the mean of the geometric distribution.  Solving for p derives
+//! the parameter from the configuration, but the code operates on \c
+//! p_comp, which is just 1-p.  Entering an overhang involves a penalty
+//! (since the probability is only one half), but not entering it
+//! involves the same penalty.  The difference in penalties is zero,
+//! which also means a perfect alignment stays at a zero score.
+//!
+//! The penalty for extending an overhang by one nucleotide is just the
+//! probability of it being longer.
 //!
 //! \param conf set of configuration parameters
 //! \param out if not NULL, receives a protocol of the calculated
@@ -69,7 +78,7 @@ void simple_adna::configure( const config::Aligner& conf, std::ostream *out )
 	if( conf.has_mean_overhang_length() )
 	{
 		double p_comp = 1 - 1 / (2*conf.mean_overhang_length() + 1) ;
-		overhang_enter_penalty = to_log_dom( p_comp / 2 ) - to_log_dom( 1 - p_comp / 2 ) ;
+		overhang_enter_penalty = 0 ;
 		overhang_ext_penalty = to_log_dom( p_comp ) ;
 	}
 	else

commit 787e8045ba12c85d2fdb6942e28b9c905e7395b1

fixed bug introduced in R1681 and buggy error handling in index.cc
git-svn-id: http://biofs04/svn/bioinf/anfo/tags/anfo-0.9.0@1685 93d9a472-daf7-0310-8f21-d7befa554562

align.h
@@ -493,9 +493,9 @@ template< typename F > void forward( const simple_adna& s, F f )
 			f( s1 ) ;
 		}
 	}
-	else if( (s.state & simple_adna::mask_dir) == 0 && !s.get_ref() )
+	else if( (s.state & simple_adna::mask_dir) == 0 && !s.get_qry().ambicode )
 	{
-		// forward dir, hit gap --> start over in reverse dir
+		// forward dir, hit gap in query --> start over in reverse dir
 		simple_adna s1 = s ;
 		s1.state = 1 ;
 		s1.ref_offs = -1 ;

configure
@@ -1,6 +1,6 @@
 #! /bin/sh
 # Guess values for system-dependent variables and create Makefiles.
-# Generated by GNU Autoconf 2.61 for anfo 0.9.0.1.
+# Generated by GNU Autoconf 2.61 for anfo 0.9.0.2.
 #
 # Report bugs to <udo_stenzel@eva.mpg.de>.
 #
@@ -574,8 +574,8 @@ SHELL=${CONFIG_SHELL-/bin/sh}
 # Identity of this package.
 PACKAGE_NAME='anfo'
 PACKAGE_TARNAME='anfo'
-PACKAGE_VERSION='0.9.0.1'
-PACKAGE_STRING='anfo 0.9.0.1'
+PACKAGE_VERSION='0.9.0.2'
+PACKAGE_STRING='anfo 0.9.0.2'
 PACKAGE_BUGREPORT='udo_stenzel@eva.mpg.de'
 
 ac_unique_file="util.cc"
@@ -1189,7 +1189,7 @@ if test "$ac_init_help" = "long"; then
   # Omit some internal or obsolete options to make the list less imposing.
   # This message is too long to be a string in the A/UX 3.1 sh.
   cat <<_ACEOF
-\`configure' configures anfo 0.9.0.1 to adapt to many kinds of systems.
+\`configure' configures anfo 0.9.0.2 to adapt to many kinds of systems.
 
 Usage: $0 [OPTION]... [VAR=VALUE]...
 
@@ -1250,7 +1250,7 @@ fi
 
 if test -n "$ac_init_help"; then
   case $ac_init_help in
-     short | recursive ) echo "Configuration of anfo 0.9.0.1:";;
+     short | recursive ) echo "Configuration of anfo 0.9.0.2:";;
    esac
   cat <<\_ACEOF
 
@@ -1330,7 +1330,7 @@ fi
 test -n "$ac_init_help" && exit $ac_status
 if $ac_init_version; then
   cat <<\_ACEOF
-anfo configure 0.9.0.1
+anfo configure 0.9.0.2
 generated by GNU Autoconf 2.61
 
 Copyright (C) 1992, 1993, 1994, 1995, 1996, 1998, 1999, 2000, 2001,
@@ -1344,7 +1344,7 @@ cat >config.log <<_ACEOF
 This file contains any messages produced by compilers while
 running configure, to aid debugging if configure makes a mistake.
 
-It was created by anfo $as_me 0.9.0.1, which was
+It was created by anfo $as_me 0.9.0.2, which was
 generated by GNU Autoconf 2.61.  Invocation command line was
 
   $ $0 $@
@@ -8051,7 +8051,7 @@ exec 6>&1
 # report actual input values of CONFIG_FILES etc. instead of their
 # values after options handling.
 ac_log="
-This file was extended by anfo $as_me 0.9.0.1, which was
+This file was extended by anfo $as_me 0.9.0.2, which was
 generated by GNU Autoconf 2.61.  Invocation command line was
 
   CONFIG_FILES    = $CONFIG_FILES
@@ -8104,7 +8104,7 @@ Report bugs to <bug-autoconf@gnu.org>."
 _ACEOF
 cat >>$CONFIG_STATUS <<_ACEOF
 ac_cs_version="\\
-anfo config.status 0.9.0.1
+anfo config.status 0.9.0.2
 configured by $0, generated by GNU Autoconf 2.61,
   with options \\"`echo "$ac_configure_args" | sed 's/^ //; s/[\\""\`\$]/\\\\&/g'`\\"
 

configure.ac
@@ -2,7 +2,7 @@
 # Process this file with autoconf to produce a configure script.
 
 AC_PREREQ(2.61)
-AC_INIT([anfo], [0.9.0.1], [udo_stenzel@eva.mpg.de])
+AC_INIT([anfo], [0.9.0.2], [udo_stenzel@eva.mpg.de])
 AC_CONFIG_SRCDIR([util.cc])
 AC_CONFIG_HEADER([config.h])
 

index.cc
@@ -82,6 +82,7 @@ FixedIndex::FixedIndex( const std::string& name, const config::Config& c, int ad
 		length = the_stat.st_size ;
 		p = mmap( 0, length, PROT_READ, MAP_SHARED, fd_, 0 ) ;
 		throw_errno_if_minus1( p, "mmapping", name.c_str() ) ;
+		p_ = p ;
 
 		if( adv ) madvise( p, length, adv ) ;
 
@@ -95,11 +96,10 @@ FixedIndex::FixedIndex( const std::string& name, const config::Config& c, int ad
 		first_level_len = 1 << (2 * ci_.wordsize()) + 1 ;
 		base = (const uint32_t*)( (const char*)p + 8 + ((3+meta_len) & ~3) ) ;
 		secondary = base + first_level_len ; 
-		p_ = p ;
 	}
 	catch(...)
 	{
-		if( p ) munmap( p, length ) ;
+		if( p_ ) munmap( (void*)p_, length ) ;
 		if( fd_ != -1 ) close( fd_ ) ;
 		throw ;
 	}