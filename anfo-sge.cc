697279f009ecf01b7d40bad03f503537004eb946
udo_stenzel <udo_stenzel@93d9a472-daf7-0310-8f21-d7befa554562>
2009-09-25 18:52:23 +0000

    added license declaration
    git-svn-id: http://biofs04/svn/bioinf/anfo/tags/anfo-0.95.0@1869 93d9a472-daf7-0310-8f21-d7befa554562

--- anfo-sge.cc
+++ anfo-sge.cc
@@ -1,3 +1,19 @@
+//    Copyright 2009 Udo Stenzel
+//    This file is part of ANFO
+//
+//    ANFO is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Foobar is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
 //! \page anfo_for_grid_engine ANFO executable for the SGE
 //! This is the version of ANFO hacked to run smoothly on hostile
 //! environments, e.g. our broken setup of the Sun Grid Engine.  These
