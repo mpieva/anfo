ced805e7b282eb181141dadaca5dee94250f66aa
udo_stenzel <udo_stenzel@93d9a472-daf7-0310-8f21-d7befa554562>
2009-10-12 13:16:22 +0000

    fixed inconsistencies in legalese
    git-svn-id: http://biofs04/svn/bioinf/anfo/tags/anfo-0.96@1906 93d9a472-daf7-0310-8f21-d7befa554562

--- index-test.cc
+++ index-test.cc
@@ -6,13 +6,13 @@
 //    the Free Software Foundation, either version 3 of the License, or
 //    (at your option) any later version.
 //
-//    Foobar is distributed in the hope that it will be useful,
+//    Anfo is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //    GNU General Public License for more details.
 //
 //    You should have received a copy of the GNU General Public License
-//    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+//    along with Anfo.  If not, see <http://www.gnu.org/licenses/>.
 
 //! \page index_test Testing the indexing phase
 //! This binary just runs the indexer on whatever input it is given,
